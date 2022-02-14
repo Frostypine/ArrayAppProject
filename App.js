1359b880c04d72ad618bca36efcdfbc4e165c074
Katie Greenwald <frostypinekng@yahoo.com>
2022-02-13 17:15:17 -0700

    nav part 2 done

--- App.js
+++ App.js
@@ -5,28 +5,37 @@
  * @format
  * @flow strict-local
  */
+ import * as React from 'react';
 
-import React from 'react';
 
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
-
-
+const Stack = createNativeStackNavigator();
 
 const App = () => {
  
 
   return (
-   <></>
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
+      
   );
 };
 
